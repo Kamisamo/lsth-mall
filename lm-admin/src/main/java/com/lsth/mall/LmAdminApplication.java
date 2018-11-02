b1dd71bfc347e090ab16e7de3ee8185712034733
Kamisamo <Kamisamo@users.noreply.github.com>
2018-11-02 14:39:56 +0800

    first

---
+++ lm-admin/src/main/java/com/lsth/mall/LmAdminApplication.java
@@ -0,0 +1,12 @@
+package com.lsth.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmAdminApplication.class, args);
+	}
+}
